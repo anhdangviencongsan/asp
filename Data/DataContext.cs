e12cdbdcb3e97a381cfb94e25724a7e1f8f2b50a
Anh Dang Vien <xengxucsoi@gmail.com>
2021-06-09 22:21:47 +0700

    config

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+
+using asp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Movie> Movies {get;set;}
+        
+    }
+}
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,7 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace asp.Models
 {
+
     public class Movie
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
+        [StringLength(50)]
+        public string Author { get; set; }
+
+
     }
 }
